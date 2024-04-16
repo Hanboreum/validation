0fb43e0022763e76609b7bd1332e8fb1b831b1be
fullm <matoma1_@naver.com>
2024-04-16 18:41:55 +0900

    user model 생성

---
+++ src/main/java/com/example/validation/model/UserRegisterRequest.java
@@ -0,0 +1,25 @@
+package com.example.validation.model;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class UserRegisterRequest {
+
+    private String name;
+    private Integer age;
+    private String email;
+    private String phoneNumber;
+    private LocalDateTime registerAt;
+
+}
