4c90620bee63e1475ac64eb1155f07b6c65c2a04
fullm <matoma1_@naver.com>
2024-04-16 18:44:25 +0900

    요청값확인

--- src/main/java/com/example/validation/controller/UserApiController.java
+++ src/main/java/com/example/validation/controller/UserApiController.java
@@ -1,4 +1,22 @@
 package com.example.validation.controller;
 
+import com.example.validation.model.UserRegisterRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/user")
 public class UserApiController {
+
+    //가입
+    @GetMapping("")
+    public UserRegisterRequest register(@RequestBody UserRegisterRequest user){
+        log.info("userRequest : {}", user);
+
+        return user;
+    }
 }
