80ca848c6cd0cf1948d2d010b7b4bcae5e6832fd
fullm <matoma1_@naver.com>
2024-04-17 10:15:41 +0900

    Api 적용

--- src/main/java/com/example/validation/controller/UserApiController.java
+++ src/main/java/com/example/validation/controller/UserApiController.java
@@ -1,5 +1,6 @@
 package com.example.validation.controller;
 
+import com.example.validation.model.Api;
 import com.example.validation.model.UserRegisterRequest;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
@@ -12,9 +13,10 @@ public class UserApiController {
 
     //가입
     @PostMapping("")
-    public UserRegisterRequest register(
+    public Api<UserRegisterRequest> register( //요청 할 때도 Api스펙을 통해 리턴.
             @Valid //검증
-            @RequestBody UserRegisterRequest user){
+            @RequestBody Api<UserRegisterRequest> user){ //요청도 Api스펙으로 요청
+        //이제 서버는 항상Api라는 스펙을 통해서만 제이슨을 보낼 수 있음
         log.info("userRequest : {}", user);
 
         return user;
