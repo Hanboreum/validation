0e675e8f91b2e4619e3fba2d373e9c33f22cbc85
fullm <matoma1_@naver.com>
2024-04-17 10:45:15 +0900

    Api형식에 맞춰 register 수정

--- src/main/java/com/example/validation/controller/UserApiController.java
+++ src/main/java/com/example/validation/controller/UserApiController.java
@@ -4,6 +4,7 @@ import com.example.validation.model.Api;
 import com.example.validation.model.UserRegisterRequest;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -19,6 +20,14 @@ public class UserApiController {
         //이제 서버는 항상Api라는 스펙을 통해서만 제이슨을 보낼 수 있음
         log.info("userRequest : {}", user);
 
-        return user;
+
+        var body = user.getData();
+        Api<UserRegisterRequest> response = Api.<UserRegisterRequest>builder()
+                .data(body)
+                .resultCode(String.valueOf(HttpStatus.OK.value()))
+                .resultMessage(HttpStatus.OK.getReasonPhrase())
+                .build();
+        //빌더 통해 해당 데이터 빌드, response응답 내림
+        return response;
     }
 }
