a210f946e48bcaede8e3f6b0680aff21284734ea
fullm <matoma1_@naver.com>
2024-04-17 10:11:51 +0900

    공통된 스펙의 클래스

---
+++ src/main/java/com/example/validation/model/Api.java
@@ -0,0 +1,30 @@
+package com.example.validation.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Api<T> {
+
+    private String resultCode;
+    private String resultMessage;
+    private T data;
+    private Error error;
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class Error{
+        //에러를 담아줄 에러 객체
+        private List<String> errorMessage;
+    }
+
+}
