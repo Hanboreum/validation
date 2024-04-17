80a5b21f18f06e934eef5cc1c4e5873dcd449d2d
fullm <matoma1_@naver.com>
2024-04-17 16:36:39 +0900

    복합 조건 메소드 이름 or nickname 둘 중 하나는 반드시 존재해야함

--- src/main/java/com/example/validation/model/UserRegisterRequest.java
+++ src/main/java/com/example/validation/model/UserRegisterRequest.java
@@ -9,6 +9,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Data
 @NoArgsConstructor
@@ -19,8 +20,9 @@ public class UserRegisterRequest {
 
    // @NotNull //not null
     //@NotEmpty // not null + not empty
-    @NotBlank //not null + not empty + not blank "  "
+   //@NotBlank //not null + not empty + not blank "  "
     private String name;
+    private String nickName; //name or nickName 둘 중 하나가 있어야 함
 
     @NotBlank
     @Size (min =1, max =12) //pw의 최소 길이는 1, 최대 길이는 12
@@ -40,21 +42,47 @@ public class UserRegisterRequest {
     @FutureOrPresent //현재 혹은 미래
     private LocalDateTime registerAt;
 
+   // @AssertFalse 해당 값이 false여야함 default return 을 true 줄 때 사용
+    @AssertTrue(message = "name or nickname 은 존재해야 합니다.")// 해당 return 이 true 일 때 실행.
+       public boolean isNameCheck(){ //is는 필수
+        if (Objects.nonNull(name) && !name.isBlank()){
+            return true;//name object가 null이 아니어야 하고, name이 blank가 아니면 통과
+        }
+        if(Objects.nonNull(nickName) && !nickName.isBlank()){
+            return true;//nickname이 not null이어야 하고, blank가 아니어야  통과
+        }
+        return false;
+    }
+
 }
 /*
-{
- "result_code" : "",
- "result+message" : "",
- "data" : {}, //오브젝트 표시
- "error" : {}
 
-  {
-    "name": "이름",
-    "password": "dfs",
+  @AssertTrue(message = "name or nickname 은 존재해야함")
+ public boolean isnameCheck(){
+     if(!name.isBlank()){
+      return true;
+     }
+     if(!nickName.isBlank()){
+      return true;
+     }
+     return false;
+    }
+    ---
+{
+  "result_code": "200",
+  "result_message": "Success",
+  "data": {
+    "name": "ㄹㄹ ",
+    "nick_name" : "ㄹㅇ",
+    "password": "123",
     "age": 30,
     "email": "name@example.com",
     "phone_number": "010-1234-5678",
-    "register_at": "2024-04-17T12:00:00"
+    "register_at": "2024-04-17T17:00:00"
+  },
+  "error": {
+    "error_message": [
+      "Error occurred during processing"
+    ]
   }
-}
- */
+} */
