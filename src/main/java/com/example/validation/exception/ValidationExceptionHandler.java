f81627db86bb0ed68e616f6288ef791c50a5ddbb
fullm <matoma1_@naver.com>
2024-04-17 11:57:00 +0900

    주석 추가

--- src/main/java/com/example/validation/exception/ValidationExceptionHandler.java
+++ src/main/java/com/example/validation/exception/ValidationExceptionHandler.java
@@ -2,12 +2,15 @@ package com.example.validation.exception;
 
 import com.example.validation.model.Api;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.el.parser.ParseException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -42,5 +45,58 @@ public class ValidationExceptionHandler {
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(errorResponse);
+
+
+    }
+
+
+}
+  /*
+        {
+    "result_code": "Bad Request",
+    "result_message": null,
+    "data": null,
+    "error": {
+        "error_message": [
+            "data.name : {  } 은 공백일 수 없습니다 "
+        ]
     }
 }
+
+         */
+/*
+{
+    "timestamp": "2024-04-17T02:47:38.820+00:00",
+    "status": 400,
+    "error": "Bad Request",
+    "path": "/api/user"
+}
+
+ */
+
+/*
+
+@ExceptionHandler(value ={ ParseException.class})
+    public ResponseEntity<Api> parseException (ParseException e) {
+        log.error("error", e);
+        String rejectedValue = e.getMessage();
+
+        // 에러 메시지 구성
+        String errorDetail = String.format("Parsing failed for value '%s'", rejectedValue);
+
+        // 에러 객체 생성
+        Api.Error error = Api.Error.builder()
+                .errorMessage(Collections.singletonList(errorDetail))
+                .build();
+
+        // API 응답 객체 생성
+        Api<Object> errorResponse = Api.builder()
+                .resultCode(String.valueOf(HttpStatus.BAD_REQUEST.value()))
+                .resultMessage(HttpStatus.BAD_REQUEST.getReasonPhrase())
+                .error(error)
+                .build();
+
+        // ResponseEntity 생성하여 반환
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
+ */
